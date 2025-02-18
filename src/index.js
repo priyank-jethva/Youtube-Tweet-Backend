f325ceb9eb7b8f6f3972e2cb9b80c42eb1902f13
Priyank1905 <priyankjethva1905@gmail.com>
2025-02-18 19:38:32 +0530

    Add video routes and update video model for file uploads

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-// require('dotenv').config({path: './env'})
-import dotenv from "dotenv";
+// require('dotenv').config({path: './.env'})
+// // require('dotenv').config()
+// console.log(process.env) // { PORT: '8000', DB_URL: 'mongodb://localhost:27017/notes' }
+// // import dotenv from "dotenv";
 import connectDB from "./db/index.js"
 import { app } from "./app.js";
 
-dotenv.config({
-    path: "./env"
-})
+// dotenv.config({
+//     path: "./env"
+// })
+
+console.log("hello");
 
 
 connectDB()
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,46 +1,51 @@
-import mongoose, { Schema } from "mongoose";
+import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new mongoose.Schema(
+const videoSchema = new Schema(
     {
-        videoFile:{
-            type : String,
-            required : true,
+        videoFile: {
+            type: {
+                url: String,
+                public_id: String,
+            },
+            required: true,
         },
-        thumbnail:{
-            type : String,
-            required : true,
+        thumbnail: {
+            type: {
+                url: String,
+                public_id: String,
+            },
+            required: true,
         },
-        title:{
-            type : String,
-            required : true,
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
         },
-        description:{
-            type : String,
-            required : true,
+        title: {
+            type: String,
+            required: true,
         },
-        duration:{
-            type : Number,
-            required : true
+        description: {
+            type: String,
+            required: true,
         },
-        views : {
-            type : Number,
-            default : 0
+        duration: {
+            type: Number,
+            required: true,
         },
-        isPublished:{
-            type : Boolean,
-            default : true
+        views: {
+            type: Number,
+            defaultValue: 0,
         },
-        owner:{
-            type : Schema.Types.ObjectId,
-            ref : "User"
-        }
+        isPublished: {
+            type: Boolean,
+            defaultValue: false,
+        },
     },
     {
-        timestamps: true
+        timestamps: true,
     }
-)
+);
 
-videoSchema.plugin(mongooseAggregatePaginate)
-
-export const Video = mongoose.model("Video",videoSchema)
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = model("Video", videoSchema);
