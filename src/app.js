04283031308297854e0f0385de8b9bb7f496ac73
Priyank1905 <priyankjethva1905@gmail.com>
2025-02-21 09:43:52 +0530

    Add tweet functionality with routes, model, and controller

--- src/app.js
+++ src/app.js
@@ -18,11 +18,13 @@ app.use(cookieParser())
 
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+
 
 //route declatration
 
 app.use("/api/v1/video", videoRouter);
-
+app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/users",userRouter);
 
 export { app }
