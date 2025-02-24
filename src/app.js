951f4db9a728a76e9be7202ae0fa459d35e06ae6
Priyank1905 <priyankjethva1905@gmail.com>
2025-02-24 21:13:11 +0530

    Add subscription routes and model for managing user subscriptions

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+
 
 
 //route declatration
@@ -26,5 +28,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/users",userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+
 
 export { app }
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,17 +1,17 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 
-const subscriptionSchema = new Schema({
-    subscriber: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    channel: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, 
+const subscriptionSchema = new Schema(
     {
-        timestamps: true
-    })
+        subscriber: {
+            type: Schema.Types.ObjectId, //one who is subscribing
+            ref: "User"
+        },
+        channel: {
+            type: Schema.Types.ObjectId, //one to whom subscriber subscribes
+            ref: "User"
+        },
+    },
+    { timestamps: true }
+);
 
-export const Subscription = mongoose.model("Subscription", subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
