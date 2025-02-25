d5046148df0832762148e4e3e0e2b632f8d356bf
Priyank1905 <priyankjethva1905@gmail.com>
2025-02-25 09:53:53 +0530

    Add like functionality with routes, controller, and model

--- src/app.js
+++ src/app.js
@@ -20,6 +20,8 @@ import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import likeRouter from "./routes/like.routes.js";
+
 
 
 
@@ -29,6 +31,8 @@ app.use("/api/v1/video", videoRouter);
 app.use("/api/v1/tweet", tweetRouter);
 app.use("/api/v1/users",userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/likes", likeRouter);
+
 
 
 export { app }
